aca5d62992952a0c09291a052c6f60a8e4e21306
hungnv132 <hungnv132@gmail.com>
2019-01-16 17:46:44 +0700

    update script

--- download_audio.py
+++ download_audio.py
@@ -1,5 +1,20 @@
-from pathlib import PosixPath
+from pathlib import Path, PosixPath
 import urllib.request
+
+home_directory = str(Path.home())
+storing_path = home_directory + '/audios/'
+links_file = PosixPath('./links.txt')
+
+
+def init_location(path):
+    p = Path(path)
+    if not p.exists():
+        p.mkdir()
+
+
+def get_audio_file_name(path):
+    last_slash_index = path.rfind('/')
+    return path[last_slash_index + 1:len(path) - 1]
 
 
 def download_audio(file_):
@@ -10,15 +25,10 @@ def download_audio(file_):
         for link in links:
             audio_name = get_audio_file_name(link)
             print("Downloading: " + audio_name)
-            urllib.request.urlretrieve(link, audio_name)
+            urllib.request.urlretrieve(link, storing_path + audio_name)
     print('Finished!')
 
 
-def get_audio_file_name(path):
-    last_slash_index = path.rfind('/')
-    return path[last_slash_index + 1:len(path) - 1]
-
-
 if __name__ == '__main__':
-    file_ = PosixPath('./links.txt')
-    download_audio(file_)
+    init_location(storing_path)
+    download_audio(links_file)
